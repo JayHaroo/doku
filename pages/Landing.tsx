05cfba8b91a471d40fda533e755cf44520e09354
M-Jey <mjeyict3@gmail.com>
2025-04-23 11:56:33 +0800

    Refactor Landing page layout for improved styling and structure

--- pages/Landing.tsx
+++ pages/Landing.tsx
@@ -8,8 +8,8 @@ export default function Landing() {
 
   if (!fontsLoaded) return null;
   return (
-    <View className="flex-1 items-center justify-center bg-black">
-      <View className="items-center justify-center">
+    <View className="flex-1 items-center justify-evenly bg-black">
+      <View className="items-center justify-center pt-[170px]">
         <Text className="text-white text-3xl font-satoshi p-5">Doku.</Text>
         <Pressable className="border-[#E68E20] border rounded-full px-4 py-2 mt-4">
           <Text className="text-[#E68E20] font-satoshi p-2">Deploy to a blockchain</Text>
@@ -17,6 +17,11 @@ export default function Landing() {
         <Pressable className="border-white border rounded-full px-4 py-2 mt-4">
           <Text className="text-white font-satoshi p-2">Create Local Contracts</Text>
         </Pressable>
+      </View>
+      <View className="pt-[120px]">
+        <Text className="text-white text-center font-satoshi p-5 mt-4 text-[10px]">
+          By using Doku, you agree to our terms of service and privacy policy.
+        </Text>
       </View>
     </View>
   );
