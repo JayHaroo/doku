2874829f51e0631a2d6da47edfb34e21e324b64b
M-Jey <mjeyict3@gmail.com>
2025-05-21 16:29:18 +0800

    Implement code changes to enhance functionality and improve performance

--- metro.config.js
+++ metro.config.js
@@ -4,21 +4,3 @@ const { withNativeWind } = require('nativewind/metro');
 const defaultConfig = getDefaultConfig(__dirname)
 
 module.exports = withNativeWind(defaultConfig, { input: './global.css' })
-
-const config = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
-  resolver: {
-    extraNodeModules: {
-      ...require("node-libs-react-native"),
-    },
-  },
-}
-
-module.exports = mergeConfig(defaultConfig, config)
--- pages/OnChainForm.tsx
+++ pages/OnChainForm.tsx
@@ -3,13 +3,9 @@ import { View, Text, Pressable, ScrollView, Image } from 'react-native';
 export default function OnChainForm() {
     return(
         <View className="flex-1 items-center justify-evenly bg-black">
-            <Pressable className='border-2 border-[#FF5C16] rounded-3xl'>
+            <Pressable className='border-2 border-[#0053FE] rounded-3xl'>
                 <View className='flex flex-row items-center p-3'>
-                    <Image 
-                    className='object-contain w-[40px] h-[40px]'
-                    source={require('../assets/metamask.png')}
-                    />
-                    <Text className='text-white px-4 font-jetbrains'>Connect to metamask</Text>
+                    <Text className='text-white px-4 font-jetbrains'>Connect wallet</Text>
                 </View>
             </Pressable>
 
