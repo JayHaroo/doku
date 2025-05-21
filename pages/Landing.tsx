9f61c72b1a46f46d4ebf6871e72d1a342bedc529
M-Jey <mjeyict3@gmail.com>
2025-05-21 16:38:46 +0800

    Refactor Landing component: update font class for buttons to improve consistency

--- pages/Landing.tsx
+++ pages/Landing.tsx
@@ -10,12 +10,12 @@ export default function Landing() {
         <Text className="text-white text-3xl font-satoshi p-5">Doku.</Text>
         <Pressable className="border-2 border-[#0053FE] border rounded-full px-4 py-2 mt-4"
           onPress={() => navigation.navigate("OnChainForm")}>
-          <Text className="text-white font-satoshi p-2">Create and deploy to BASE</Text>
+          <Text className="text-white font-jetbrains p-2">Create and deploy to BASE</Text>
         </Pressable>
         <Pressable className="border-white border rounded-full px-4 py-2 mt-4"
           onPress={() => navigation.navigate("LocalForm")}
         >
-          <Text className="text-white font-satoshi p-2">Create an agreement</Text>
+          <Text className="text-white font-jetbrains p-2">Create an agreement</Text>
         </Pressable>
       </View>
       <View className="pt-[120px]">
